7ac4c8ece0d28342d36d72c12a9e17be36077b53
Prince Premnath <Prince Premnath>
2017-06-11 21:36:42 +0530

    Code Cleanup

--- testB/AsgSearch.web/Controllers/HomeController.cs
+++ testB/AsgSearch.web/Controllers/HomeController.cs
@@ -11,15 +11,9 @@ namespace AsgSearch.web.Controllers
 {
     public class HomeController : Controller
     {
-        IStackExchangeSearch search;
-        QueryService qService;
+      
 
-        public HomeController()
-        {
-            search = new StackExchangeSearch();
-            //qService = new QueryService(new DAL.DALContext());
-            qService = null;
-        }
+        
         public ActionResult Index()
         {
             ViewBag.Title = "Home";
@@ -45,32 +39,5 @@ namespace AsgSearch.web.Controllers
             return View();
         }
 
-        //public ActionResult Search(string searchKey)
-        //{
-        //    var p = search.Search(searchKey);
-        //    List<SearchResult> results = new List<SearchResult>();
-
-        //    for (int i = 0; i < p.Data.Items.Length; i++)
-        //    {
-        //        results.Add(new SearchResult
-        //        {
-        //            Title = p.Data.Items[i].Title,
-        //            Avatar = p.Data.Items[i].Owner.ProfileImage,
-        //            OwnerName = p.Data.Items[i].Owner.DisplayName,
-        //            Tags = p.Data.Items[i].Tags != null ? String.Join(",", p.Data.Items[i].Tags.Select(key => "'" + key + "'")).ToString() : string.Empty,
-        //            AcceptedAnsId = p.Data.Items[i].AcceptedAnswerId,
-        //            DateCreated = p.Data.Items[i].CreationDate
-
-        //        });
-        //    }
-        //    qService.SaveQuery(searchKey, System.DateTime.Now, results);
-        //    return View("_List", p.Data.Items);
-        //}
-
-        //public ActionResult ListQuery()
-        //{
-        //    var Quries = qService.GetQueries();
-        //    return View("_QueryList", Quries);
-        //}
     }
 }
